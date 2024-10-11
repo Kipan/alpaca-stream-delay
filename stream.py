27a0221cf09543f5c79524e1d391d6823245548a
Nathan P <nmpak2000@gmail.com>
2024-10-10 23:17:51 -0500

    initial commit

---
+++ stream.py
@@ -0,0 +1,77 @@
+import logging
+import threading
+import time
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from alpaca.data.live import CryptoDataStream, StockDataStream
+from alpaca.data.enums import DataFeed
+from datetime import timedelta, datetime, timezone
+import pandas as pd
+from config import API_KEY, SECRET_KEY
+
+class Stream():
+    def __init__(self):
+        self.stock_conn = StockDataStream(API_KEY, SECRET_KEY)
+        self.crypto_conn = CryptoDataStream(API_KEY, SECRET_KEY)
+
+
+    async def on_recv(self, q):
+        print('quote', q)
+        if q.timestamp.replace(tzinfo=None) - self.start >= timedelta(seconds=20):
+            print("its been 20 seconds biatch")
+            await self.stop()
+            return
+
+
+    def add_stream(self, exchange, ticker):
+        if exchange == 'stock':
+            conn = self.stock_conn
+        elif exchange == 'crypto':
+            conn = self.crypto_conn
+        else:
+            print("exchange must be stock or crypto")
+            return
+
+        try:
+            self.consumer_thread(conn, ticker)
+        except KeyboardInterrupt:
+            print("Interrupted execution by user")
+            conn.stop_ws()
+            exit(0)
+        except Exception as e:
+            print("You got an exception: {} during execution. continue "
+                  "execution.".format(e))
+            # let the execution continue
+
+
+    def consumer_thread(self, conn, ticker):
+
+        conn.subscribe_quotes(self.on_recv, ticker)
+        #conn.run()
+
+    def set_tickers(self, dict: dict):
+        # dict follows {'crypto':[ticker], 'stock':[ticker]}
+        for exchange, item_arr in dict.items():
+            for ticker in item_arr:
+                self.add_stream(exchange, ticker)
+    
+
+    def run(self):
+        self.start = datetime.now(timezone.utc).replace(tzinfo=None)
+        self.crypto_conn.run()
+        self.stock_conn.run()
+
+    async def stop(self):
+        await self.crypto_conn.close()
+        await self.stock_conn.close()
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                        level=logging.INFO)
+    str = Stream()
+    str.set_tickers({
+        'crypto': ['BTC/USD', 'ETH/USD']
+    })
+    str.run()
+    print("hhhhhhhhhhhhhhhhhhhhhh")
+    time.sleep(5)  # give the initial connection time to be established
---
+++ stream_delay.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import time
+from alpaca.data import CryptoHistoricalDataClient, StockHistoricalDataClient, OptionHistoricalDataClient
+from alpaca.data.requests import StockBarsRequest, StockQuotesRequest
+from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
+from alpaca.data.enums import Adjustment
+from datetime import datetime, timedelta, timezone
+from Stream import Stream
+from config import API_KEY, SECRET_KEY
+
+class PoorStream(Stream):
+    DELAY_MINUTES = 15
+
+    def __init__(self, window, bar_length, symbol):
+        super().__init__()
+        self.window = window
+        self.bar_length = pd.to_timedelta(bar_length)
+        self.symbol = symbol
+        self.raw_data = None
+        self.last_bar = None
+        self.tick_data = pd.DataFrame()
+        self.streamed_15 = False # we'll check this during the on_success method
+        self.stock_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
+        self.set_tickers({'stock':[self.symbol]})
+
+    def create_timedelta(self, bar_length, window):
+        if bar_length == pd.to_timedelta("1d"):
+            return timedelta(days=window)
+        elif bar_length == pd.to_timedelta("1h"):
+            return timedelta(hours=window)
+        elif bar_length == pd.to_timedelta("1min"):
+            return timedelta(minutes=window)
+        else:
+            raise ValueError("Invalid unit. Use 'day', 'hour', or 'minute'.")
+        
+    async def on_recv(self, q):
+        print('quote', q)
+        recent_tick = q.timestamp.replace(tzinfo=None)
+        df = pd.DataFrame({self.symbol:(q.ask_price + q.bid_price)/2}, 
+                          index = [recent_tick])
+        self.tick_data = pd.concat([self.tick_data, df])
+        if self.streamed_15 and recent_tick - self.last_bar > self.bar_length:
+            self.resample_and_join()
+
+        elif not self.streamed_15 and recent_tick - self.start \
+            >= timedelta(minutes=self.DELAY_MINUTES):
+            print("its been {} minutes".format(self.DELAY_MINUTES))
+
+    def resample_and_join(self):
+        self.raw_data = pd.concat([self.raw_data, self.tick_data.resample(self.bar_length, 
+                                                                          label="right").last().ffill().iloc[:-1]])
+        self.tick_data = self.tick_data.iloc[-1:]
+        self.last_bar = self.raw_data.index[-1]
+
+    def get_most_recent(self):
+        while True:
+            time.sleep(2)
+            now = datetime.now(timezone.utc).replace(tzinfo=None)
+            now = now - timedelta(microseconds = now.microsecond) - timedelta(minutes=self.DELAY_MINUTES)
+            past = now - self.create_timedelta(self.bar_length, self.window)
+            request_params = StockBarsRequest(
+                symbol_or_symbols = self.symbol,
+                timeframe = TimeFrame.Minute,
+                start = past,
+                end = now,
+                adjustment = "all"
+            )
+            df = self.stock_client.get_stock_bars(request_params=request_params).df
+            df = df.reset_index()
+            df = df[["timestamp", "close"]]
+            df = df.set_index("timestamp")
+            df.rename(columns = {"close":self.symbol}, inplace = True)
+            df = df.resample(self.bar_length, label = "right").last().dropna().iloc[:-1]
+            self.raw_data = df.copy()
+            self.last_bar = self.raw_data.index[-1]
+            if pd.to_datetime(datetime.now(timezone.utc).replace(tzinfo=None)).tz_localize("UTC") - self.last_bar + \
+                pd.to_timedelta("{}m".format(self.DELAY_MINUTES)) < self.bar_length:
+                break
+
+if __name__ == "__main__":
+    ps = PoorStream(window=200, bar_length="1d", symbol="SPY")
+    ps.get_most_recent()
